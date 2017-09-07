12bc2e17743aec73b149d661fa0466e7f1cad9ce
Alva Chien <alvachien@163.com>
2017-09-07 14:12:03 +0800

    Remove applicationinsight

--- acquizapi/Program.cs
+++ acquizapi/Program.cs
@@ -17,7 +17,6 @@ namespace acquizapi
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
