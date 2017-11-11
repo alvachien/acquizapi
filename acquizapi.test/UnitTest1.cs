0e527f8987b770c3506b9df772e37b8f8cac77d0
Alva Chien <alvachien@live.com>
2017-11-11 12:27:18 +0800

    Add Unit Test

---
+++ acquizapi.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using acquizapi.Models;
+using acquizapi.Controllers;
+
+namespace acquizapi.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
